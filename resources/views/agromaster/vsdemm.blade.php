d24c6631feba2f51b1fb78919015e0a1ab8768c4
GalychIvan <galych.ivan@ukr.net>
2023-12-01 22:09:33 +0200

    add

--- resources/views/agromaster/vsdemm.blade.php
+++ resources/views/agromaster/vsdemm.blade.php
@@ -9,5 +9,16 @@
 
 @section('content')
 
+    <div class="container overflow-hidden text-center">
+        <div class="row gx-5">
+            <div class="col">
+                <div class="p-3"><img src="{{'/uploads/vsdemm/1.png'}}" alt="" ></div>
+            </div>
+            <div class="col">
+                <div class="p-3"><img src="{{'/uploads/vsdemm/2.jpg'}}" alt="" ></div>
+            </div>
+        </div>
+    </div>
+
 
 @endsection
